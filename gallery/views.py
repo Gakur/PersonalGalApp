e6bde61c251676c49788f9bbb2acc3b10617ae6b
Peter Gakure <petergakure97@gmail.com>
2021-10-08 15:37:56 +0300

    Update search view function

--- gallery/views.py
+++ gallery/views.py
@@ -21,3 +21,18 @@ def by_image(request, image_id, category_name):
         raise Http404()
     return render(request,"image.html",{"locations": locations}, {"categories": categories}, {"title": title}, {"image": image})
 
+def search(request):
+    locations = location.objects.all()
+    categories = category.objects.all()
+    title = 'Get Images Page'
+    if 'image_category' in request.GET and request.GET['image_category']:
+        search_term = request.GET.get('image_category')
+        found_results = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        print(search_term)
+        print(found_results)
+
+        return render(request, 'search.html',{'title':title,'images': found_results, 'message': message, 'categories': categories, "locations":locations})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search_image.html',{"message": message})
