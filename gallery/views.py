8cf1cfd195ed1e026f5ce9805f2b1403ba94d4a7
Peter Gakure <petergakure97@gmail.com>
2021-10-08 15:30:06 +0300

    Update by_image view function

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
-from . models import location, category, image
+from django.http import HttpResponse, Http404
+from . models import location, category, Image
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def welcome(request):
-    images = image.get_all_images()
+    image = Image.get_all_images()
     locations = location.objects.all()
     title = 'Welcome Page'
-    return render(request, 'welcome.html', {"images": images}, {"locations": locations}, {"title": title})
-    
+    return render(request, 'welcome.html', {"image": image}, {"locations": locations}, {"title": title})
+
+def by_image(request, image_id, category_name):
+    locations = location.objects.all()
+    categories = Image.objects.filter(name = category_name)
+    title = 'Images Page'
+
+    try:
+        image = Image.objects.get(id = image_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"image.html",{"locations": locations}, {"categories": categories}, {"title": title}, {"image": image})
 
