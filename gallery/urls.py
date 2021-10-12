70a448e257657b4ba7ac6810bafda9364d452485
Peter Gakure <petergakure97@gmail.com>
2021-10-12 14:35:54 +0300

    Update modified file

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 from . import views
@@ -5,7 +6,9 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome, name = 'welcome'),
-    path('image/',views.by_image, name = 'by_image'),
+    path('by_image/',views.by_image, name = 'image'),
     path('search/',views.search, name = 'search_image'),
-    path('location/(\d+)',views.filter_by_location, name = 'filter_by_location')
+    path('location/(?P<image_location>\d+)',views.filter_by_location, name = 'location')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
