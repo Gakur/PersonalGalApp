147d4d181538fdddeefd03a221862a90eaad823e
Peter Gakure <petergakure97@gmail.com>
2021-10-13 09:12:30 +0300

    update modified file

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome, name = 'welcome'),
-    path('image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.by_image, name = 'by_image'),
+    path('image/(<category_name>\w+)/(<image_id>\d+)',views.by_image, name = 'by_image'),
     path('search/',views.search, name = 'search_image'),
-    path('location/(?P<image_location>\d+)',views.filter_by_location, name = 'location')
+    path('location/(<image_location>\d+)',views.filter_by_location, name = 'location')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
